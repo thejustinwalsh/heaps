1b32a9cf39dd66e747d0cd86356a6748b5ab8c01
Nicolas Cannasse <ncannasse@gmail.com>
2014-11-21 17:20:21 +0100

    backported PAK support

--- hxd/Res.hx
+++ hxd/Res.hx
@@ -4,21 +4,27 @@ package hxd;
 @:build(hxd.res.FileTree.build())
 #end
 class Res {
-	
+
 	#if !macro
 	public static function load(name:String) {
 		return loader.load(name);
 	}
 	#end
-	
-	public static macro function initEmbed() {
-		return macro hxd.Res.loader = new hxd.res.Loader(hxd.res.EmbedFileSystem.create());
+
+	public static macro function initEmbed(?options:haxe.macro.Expr.ExprOf<hxd.res.EmbedOptions>) {
+		return macro hxd.Res.loader = new hxd.res.Loader(hxd.res.EmbedFileSystem.create(null,$options));
 	}
 
 	public static macro function initLocal() {
-		var dir = haxe.macro.Compiler.getDefine("resourcesPath");
+		var dir = haxe.macro.Context.definedValue("resourcesPath");
 		if( dir == null ) dir = "res";
 		return macro hxd.Res.loader = new hxd.res.Loader(new hxd.res.LocalFileSystem($v{dir}));
 	}
-	
+
+	public static macro function initPak() {
+		var dir = haxe.macro.Context.definedValue("resourcesPath");
+		if( dir == null ) dir = "res";
+		return macro hxd.Res.loader = new hxd.res.Loader(new hxd.fmt.pak.FileSystem($v{dir}+".pak"));
+	}
+
 }
